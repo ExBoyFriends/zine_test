6f45b339caa9d9a3ae493dcdbfb2f19d98b7f579
ExBoyFriends <tomboy.maker@gmail.com>
2026-01-21 01:00:31 +0900

    lastPage.js を更新

--- js/chapter1/lastPage.js
+++ js/chapter1/lastPage.js
@@ -1,22 +1,21 @@
 export function initLastPage(lastImg, nextBtn, getCurrentPage, totalPages){
-  let isShifted = false;
-  const maxShift = lastImg.clientWidth / 2;
+  let isShifted=false;
+  const maxShift = lastImg.clientWidth/2;
 
-  lastImg.addEventListener('click', () => {
-    if(getCurrentPage() !== totalPages -1) return;
-
-    lastImg.style.transition = 'transform 0.3s ease-out';
+  lastImg.addEventListener('click', ()=>{
+    if(getCurrentPage()!==totalPages-1) return;
+    lastImg.style.transition='transform 0.3s ease-out';
 
     if(!isShifted){
-      lastImg.style.transform = `translateX(${-maxShift}px)`;
-      nextBtn.style.pointerEvents = 'all'; // ボタン操作可能
-      isShifted = true;
+      lastImg.style.transform=`translateX(${-maxShift}px)`;
+      nextBtn.style.pointerEvents='all';
+      isShifted=true;
     } else {
-      lastImg.style.transform = 'translateX(0)';
-      nextBtn.style.pointerEvents = 'none';
-      isShifted = false;
+      lastImg.style.transform='translateX(0)';
+      nextBtn.style.pointerEvents='none';
+      isShifted=false;
     }
   });
 
-  nextBtn.addEventListener('click', () => { window.location.href = 'chapter2.html'; });
+  nextBtn.addEventListener('click', ()=>window.location.href='chapter2.html');
 }
