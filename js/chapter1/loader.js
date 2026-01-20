23ccf1f3a2918beeec0c4c8d4e2c7c8ca0a8270e
ExBoyFriends <tomboy.maker@gmail.com>
2026-01-20 23:57:06 +0900

    loader.js を更新

--- js/chapter1/loader.js
+++ js/chapter1/loader.js
@@ -1,12 +1,13 @@
 export function initLoader(pages, loader, dotsContainer){
   window.addEventListener('load', ()=>{
-    pages[0].classList.add('first-load','active');
-    loader.style.display = 'block';
+    const firstPage = pages[0];
+    firstPage.classList.add('first-load','active');
+    loader.style.display='block';
 
     setTimeout(()=>{
-      loader.style.display = 'none';
-      pages[0].classList.remove('first-load');
-      pages[0].style.transition = 'opacity 5.2s ease';
+      loader.style.display='none';
+      firstPage.classList.remove('first-load');
+      firstPage.style.transition='opacity 5.2s ease';
       dotsContainer.classList.add('visible');
     },7280);
   });
