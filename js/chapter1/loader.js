8f30edaca2e62e4b9a64a5a386e18adda9e4bc71
ExBoyFriends <tomboy.maker@gmail.com>
2026-01-21 01:21:35 +0900

    loader.js を更新

--- js/chapter1/loader.js
+++ js/chapter1/loader.js
@@ -1,18 +1,28 @@
 export function initLoader(pages, loader, dotsContainer){
   window.addEventListener('load', ()=>{
     const firstPage = pages[0];
-    firstPage.classList.add('first-load','active');
 
-    loader.style.display='block';
-    loader.style.opacity='1';
+    // 初期設定
+    firstPage.classList.add('first-load','active'); // opacity:0
+    loader.style.display = 'block';
+    loader.style.opacity = '1';
+    loader.style.animationPlayState = 'running';
 
-    // 古い挙動通り 2秒後に loader をフェードアウト
+    // 2秒後に loader をフェードアウト
     setTimeout(()=>{
-      loader.style.transition='opacity 0.5s ease';
-      loader.style.opacity='0';
-      setTimeout(()=>loader.style.display='none', 500);
+      // loader フェードアウト
+      loader.style.transition = 'opacity 0.5s ease';
+      loader.style.opacity = '0';
 
+      // loader 消去
+      setTimeout(()=> loader.style.display='none', 500);
+
+      // ドット表示
       dotsContainer.classList.add('visible');
-    },2000);
+
+      // Safari 等で transition が確実に発火するように reflow
+      firstPage.getBoundingClientRect(); // 強制再描画
+      firstPage.classList.remove('first-load'); // 7.28秒の CSS フェード開始
+    }, 2000);
   });
 }
