4e29378a959eef0bc09e4ac8d5f1373f34480bc8
ExBoyFriends <tomboy.maker@gmail.com>
2026-01-20 23:58:01 +0900

    lastPage.js を更新

--- js/chapter1/lastPage.js
+++ js/chapter1/lastPage.js
@@ -2,12 +2,12 @@ export function initLastPage(topImg, nextBtn, getCurrentPage, pages){
   const maxShift = topImg.clientWidth/2;
   let isShifted=false;
 
-  topImg.style.transform = 'translateX(0)'; // 初期中央
+  topImg.style.transform='translateX(0)'; // 初期中央
   topImg.addEventListener('click',()=>{
-    if(getCurrentPage() !== pages.length-1) return;
+    if(getCurrentPage()!==pages.length-1) return;
     topImg.style.transition='transform 0.3s ease-out';
     if(!isShifted){
-      topImg.style.transform = `translateX(${-maxShift}px)`;
+      topImg.style.transform=`translateX(${-maxShift}px)`;
       nextBtn.style.pointerEvents='auto';
     }else{
       topImg.style.transform='translateX(0)';
