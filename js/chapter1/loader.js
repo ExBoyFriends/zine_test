0f114d5e66c1d1aac16f9678ff76e6b710f40acb
ExBoyFriends <tomboy.maker@gmail.com>
2026-01-21 00:35:11 +0900

    loader.js を更新

--- js/chapter1/loader.js
+++ js/chapter1/loader.js
@@ -2,20 +2,17 @@ export function initLoader(pages, loader, dotsContainer){
   window.addEventListener('load', ()=>{
     const firstPage = pages[0];
     firstPage.classList.add('first-load','active');
-    
-    loader.style.display='block';
-    loader.style.opacity = '1'; // 初期表示確実に
 
-    // 初回フェード
+    loader.style.display='block';
+    loader.style.opacity='1';
+
     setTimeout(()=>{
       loader.style.transition='opacity 0.5s ease';
       loader.style.opacity='0';
-      firstPage.classList.remove('first-load');
-      
-      dotsContainer.classList.add('visible');
-      
-      // loader を完全削除
+
       setTimeout(()=>loader.style.display='none', 500);
+
+      dotsContainer.classList.add('visible');
     }, 7280);
   });
 }
