592a28e4d963c3482dd88262b4a02f99eca188c3
ExBoyFriends <tomboy.maker@gmail.com>
2026-01-21 00:38:52 +0900

    main.js を更新

--- js/chapter1/main.js
+++ js/chapter1/main.js
@@ -4,14 +4,14 @@ import { initLastPage } from './lastPage.js';
 
 const pages=document.querySelectorAll('.carousel-page');
 const dots=document.querySelectorAll('.dot');
-const wrapper=document.querySelector('.carousel-wrapper');
+const dotsContainer=document.querySelector('.dots');
 const loader=document.getElementById('loader');
+const wrapper=document.querySelector('.carousel-wrapper');
 const lastImg=document.querySelector('.last-img.top');
 const nextBtn=document.getElementById('next-chapter-btn');
-const dotsContainer=document.querySelector('.dots');
 
 initLoader(pages, loader, dotsContainer);
-const carousel=initCarousel(wrapper, pages, dots);
-initLastPage(lastImg, nextBtn, carousel.getCurrentPage, pages);
 
-nextBtn.addEventListener('click',()=>{ window.location.href='chapter2.html'; });
+const carousel = initCarousel(wrapper, pages, dots);
+
+initLastPage(lastImg, nextBtn, carousel.getCurrentPage, pages.length);
