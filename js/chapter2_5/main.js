304ab0d07d3dd8dc429c4253914bba89eb64cb23
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-01 16:14:14 +0900

    Update main.js

--- js/chapter2_5/main.js
+++ js/chapter2_5/main.js
@@ -3,15 +3,14 @@ import { state } from "./state.js";
 import { showPage } from "./view.js";
 import { initTapInteraction } from "./interaction.js";
 
-// 初期ページ表示
-showPage(state.index);
-
-// 操作開始
-initTapInteraction();
-
-// フェードイン開始
 window.addEventListener("load", () => {
   requestAnimationFrame(() => {
     document.body.classList.add("ready");
+
+    // フェードイン完了後に最初のページを出す
+    setTimeout(() => {
+      showPage(state.index);
+      initTapInteraction();
+    }, 1800); // bodyのtransition時間と合わせる
   });
 });
