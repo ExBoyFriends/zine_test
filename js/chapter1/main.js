d9dc78df53e7d1e053455be9f7ee57aa38b43dda
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-01 18:14:00 +0900

    Update main.js

--- js/chapter1/main.js
+++ js/chapter1/main.js
@@ -1,14 +1,27 @@
-import { initLoader } from './loader.js';
-import { initCarousel } from './carousel.js';
-import { initLastPage } from './lastPage.js';
+import { initLoader } from "./loader.js";
+import { initCarousel } from "./carousel.js";
+import { initLastPage } from "./lastPage.js";
 
-const pages = document.querySelectorAll('.carousel-page');
-const wrapper = document.querySelector('.carousel-wrapper');
-const loader = document.getElementById('loader');
-const dots = document.querySelector('.dots');
-const lastWrapper = document.querySelector('.last-img-wrapper');
+const pages       = document.querySelectorAll(".carousel-page");
+const wrapper     = document.querySelector(".carousel-wrapper");
+const loader      = document.getElementById("loader");
+const dots        = document.querySelector(".dots");
+const lastWrapper = document.querySelector(".last-img-wrapper");
 
-initLoader(pages, loader, dots);
+/* =====================
+   Chapter1 Start Logic
+===================== */
+
+function startChapter1() {
+  pages[0]?.classList.add("active");
+  dots?.classList.add("visible");
+}
+
+/* =====================
+   Init
+===================== */
+
+initLoader(loader, startChapter1);
 
 const carousel = initCarousel(wrapper, pages);
 
