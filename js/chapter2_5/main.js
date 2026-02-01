22d054f857e8ffe9824f4a200dc9e81986a10035
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-01 16:07:43 +0900

    Update main.js

--- js/chapter2_5/main.js
+++ js/chapter2_5/main.js
@@ -1,14 +1,14 @@
 // js/chapter2_5/main.js
-
 import { state } from "./state.js";
 import { showPage } from "./view.js";
 import { initTapInteraction } from "./interaction.js";
 
-// 初期表示
+// 初期ページ表示
 showPage(state.index);
 
 // 操作開始
 initTapInteraction();
+
 // フェードイン開始
 window.addEventListener("load", () => {
   requestAnimationFrame(() => {
