3e48ca5e8ffdc4fb9272a9ccfb8a45e299f7216b
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-01 15:56:24 +0900

    Create loader.js

---
+++ js/chapter2_5/loader.js
@@ -0,0 +1,17 @@
+export function initLoader(loader) {
+  if (!loader) return;
+
+  // 初期状態
+  loader.classList.remove("hide");
+  loader.style.display = "block";
+
+  // 一定時間後にフェードアウト
+  setTimeout(() => {
+    loader.classList.add("hide");
+
+    // 完全にDOMから切る
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 3500);
+  }, 1200);
+}
