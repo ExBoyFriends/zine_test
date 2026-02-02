e78e9db8f3c50b87d1aec33e889a1bd8fa96a5e3
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-02 21:38:11 +0900

    Update main.js

--- js/chapter2_5/main.js
+++ js/chapter2_5/main.js
@@ -3,26 +3,15 @@ import { state } from "./state.js";
 import { showPage } from "./view.js";
 import { initTapInteraction } from "./interaction.js";
 
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
+const loader = document.getElementById("loader");
 
-  initLoader(loader, () => {
+initLoader(loader, () => {
+  // ★ loader.js と完全同期
+  showPage(state.index);
+  initTapInteraction();
 
-    // loaderが完全に消え、描画が1フレーム確定してから
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-
-        // 初期ページ表示（chapter1と同タイミング）
-        showPage(state.index);
-        initTapInteraction();
-
-        // dots は少し遅れて
-        setTimeout(() => {
-          document.querySelector(".dots")?.classList.add("visible");
-        }, 300);
-
-      });
-    });
-
-  });
+  // dots だけ少し遅らせる
+  setTimeout(() => {
+    document.querySelector(".dots")?.classList.add("visible");
+  }, 300);
 });
