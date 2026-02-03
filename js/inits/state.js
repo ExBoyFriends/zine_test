363baa9830e271759a1a7f772588d5fa6544dbf3
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-03 23:01:36 +0900

    Update and rename state.js to state.js

--- js/inits/state.js
+++ js/inits/state.js
@@ -1,4 +1,4 @@
-//chapter2_5/state.js
+// utils/state.js
 
 export const state = {
   index: 0,
@@ -9,3 +9,8 @@ export const state = {
 export function resetTextState() {
   state.showingText = false;
 }
+
+export function updateState(newState) {
+  Object.assign(state, newState);  // stateオブジェクトを更新
+}
+
