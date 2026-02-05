59ce3c569c098e2dbbb0e67586f1a96d56e0e392
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-05 11:01:10 +0900

    Update interaction.js

--- js/chapter2_5/interaction.js
+++ js/chapter2_5/interaction.js
@@ -1,4 +1,4 @@
-// chapter2_5/interaction.js
+　// chapter2_5/interaction.js
 import { state, resetTextState } from "../utils/state.js";
 import { showPage, showText, hideText, getPages } from "./view.js";
 
@@ -59,4 +59,5 @@ function goPrev() {
   state.index--;
   resetTextState();
   showPage(state.index);  // 前のページを表示
-}
+}　　　
+
