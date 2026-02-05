688a339bc0c40e6aa07922e5f554e8fff4e2588e
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-05 16:26:21 +0900

    Update main.js

--- js/chapter1/main.js
+++ js/chapter1/main.js
@@ -16,17 +16,22 @@ const wrapper = document.querySelector(".carousel-wrapper");
 const pages   = document.querySelectorAll(".carousel-page");
 const last    = document.getElementById("last-page");
 
-initLoader(loader, () => {
-  state.index = 0;
 
+// ローダー表示 → フェードイン → チャプター開始
+initLoader(loader, () => {
+  state.index = 0; // 初期化
+
+  // 冒頭フェードイン
   fadeInStart();
 
   startChapter({
     chapter,
     dots,
     onStart() {
+      // カルーセル初期化
       const carousel = initCarousel(wrapper, pages);
 
+      // 最後のページのタップ操作（フェードアウト遷移含む）
       initLastPage(
         last,
         () => carousel.getCurrentPage(),
@@ -35,4 +40,3 @@ initLoader(loader, () => {
     }
   });
 });
-
