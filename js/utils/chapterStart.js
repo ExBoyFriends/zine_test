1ac921e494fd792e1392f5e17470b88af585816b
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-11 20:04:47 +0900

    Update chapterStart.js

--- js/utils/chapterStart.js
+++ js/utils/chapterStart.js
@@ -1,10 +1,12 @@
 // utils/chapterStart.js
-
 export function startChapter({ chapter, dots, onStart }) {
-  // 1. 本編とドットを表示（CSS側の 2.8s transition が発動する）
   chapter?.classList.add("visible");
   dots?.classList.add("visible");
 
-  // 2. 準備完了を通知
-  onStart?.();
+  // 2フレーム待つ
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      onStart?.();
+    });
+  });
 }
