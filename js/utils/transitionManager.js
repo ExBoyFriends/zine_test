2b998f953c756a21ea768035fb7cbdcb28ddca27
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-11 17:51:49 +0900

    Create transitionManager.js

---
+++ js/utils/transitionManager.js
@@ -0,0 +1,47 @@
+// utils/transitionManager.js
+import { fadeOutAndGo } from "./fade.js";
+
+export function createTransitionManager({
+  nextUrl,
+  autoDelay = null,
+  canAdvance = () => true
+}) {
+  let timer = null;
+  let transitioning = false;
+
+  function startAuto() {
+    if (!autoDelay) return;
+
+    timer = setTimeout(() => {
+      if (transitioning) return;
+      if (!canAdvance()) return;
+      goNext();
+    }, autoDelay);
+  }
+
+  function resetAuto() {
+    if (!autoDelay) return;
+    if (timer) clearTimeout(timer);
+    startAuto();
+  }
+
+  function stopAuto() {
+    if (timer) clearTimeout(timer);
+  }
+
+  function goNext() {
+    if (transitioning) return;
+    transitioning = true;
+
+    fadeOutAndGo(() => {
+      location.href = nextUrl;
+    });
+  }
+
+  return {
+    startAuto,
+    resetAuto,
+    stopAuto,
+    goNext
+  };
+}
